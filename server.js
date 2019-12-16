dc77b9180735b821b0aca32ffb3259be1424c3f6
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-12-16 15:42:22 +0700

    Prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const colors = require('colors');
 const morgan = require('morgan');
+const path = require('path');
 
 const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/errorHandler');
@@ -28,6 +29,16 @@ app.use('/api/v1/logs', logs);
 
 // Error handler middleware
 app.use(errorHandler);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Port
 const port = process.env.PORT || 5000;
